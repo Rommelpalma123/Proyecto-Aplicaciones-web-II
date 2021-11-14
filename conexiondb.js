8ffcd357bc9f50b7667bfead4bdd70b1eeaf9747
JonaEspinales <79060601+JonaEspinales@users.noreply.github.com>
2021-11-13 22:14:42 -0500

    comentario de  codigo

--- conexiondb.js
+++ conexiondb.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const { mongo_url } = require('./config');   
+const mongoose = require('mongoose');// requerir una libreria de  mongosse para conecctarse ala base de dato 
 
+const { mongo_url } = require('./config');   // en carpeta config  se estan guardando las credenciales de la base de  dato 
 
+    // esta requeriendo la libreria  de  mongoose  para conectarse a la base de dato por medio de las credenciales 
     mongoose.connect(mongo_url,{ useNewUrlParser: true, useUnifiedTopology: true},)
     .then(()  => console.log('Connected to database mongodb'))
     .catch(e  => console.log('error connect to database',e))   
--- models/historial.model.js
+++ models/historial.model.js
@@ -1,11 +1,12 @@
-const { Schema } = require('mongoose');
-const mongoose = require('mongoose');
+const { Schema } = require('mongoose'); // requeriendo la libreria schema  para crear objetos  
+const mongoose = require('mongoose');  //  requerir una libreria de  mongosse para conecctarse ala base de dato 
 // creamos arreglos
+
 const Historial = new Schema(
 
     {
-        number:  {type: String},
-        message: {type: String},
+        number:  {type: String},  //Craando objetos  historial 
+        message: {type: String},  
         today:   {type: String},
 
     },
@@ -13,5 +14,5 @@ const Historial = new Schema(
     { timestamp: { createdAt: true, updatedAt: true} }
 )
 
-module.exports = mongoose.model("historia", Historial)
+module.exports = mongoose.model("historia", Historial) //requeriendo el objeto historial en la base de dato 
 
