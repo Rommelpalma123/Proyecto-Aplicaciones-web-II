40a7045fd306c2fb7dd0789fac7a16064a336181
gustavo reyes <gustavino182@gmail.com>
2022-01-06 12:07:05 -0500

    commit microservicioss

---
+++ microservicio/user/controllers/user.js
@@ -0,0 +1,42 @@
+const User = require('../models/user');
+
+const getmessage = async ( req, res) =>
+{
+    
+    res.send("esta ruta microservicios users");
+}
+
+const getUsers = async ( req, res) =>
+{
+    const users = await User.find();
+    res.json(users);
+}
+
+const createUser = async (req, res) => 
+{
+    const { nombre, description } = req.body;
+    const user = new User({nombre, description});
+    await user.save();
+    res.json({status: 'User Saved'});
+}
+
+const getUser = async (req, res) => 
+{
+    const user = await User.findById(req.params.id);
+    res.json(user);
+}
+
+const deleteUser = async (req, res) => 
+{
+    await User.findByIdAndRemove(req.params.id);
+    res.json({status: 'user Deleted'});
+}
+
+const updateUser = async (req, res) => 
+{
+    const { name, description } = req.body;
+    const newUser = {name, description};
+    await User.findByIdAndUpdate(req.params.id, newUser);
+    res.json({status: 'User Updated'});
+}
+module.exports = { updateUser, getUsers, createUser, getUser, deleteUser, getmessage };
---
+++ microservicio/user/routes/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { getUsers, createUser, getUser, deleteUser, updateUser, getmessage } = require('../controllers/user')
+
+const router = express.Router();
+
+// GET all Tasks
+router.get('/users', getUsers);
+
+router.get('/', getmessage);
+
+// GET all Tasks
+router.get('/user/:id', getUser);
+
+// ADD a new task
+router.post('/user', createUser);
+
+// UPDATE a new task
+router.put('/user/:id', updateUser);
+
+router.delete('/user/:id', deleteUser);
+
+module.exports = router;
